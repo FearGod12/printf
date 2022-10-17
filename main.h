7abccadb9cc8e7c18a6336c02071de59b8793a43
FearGod12 <onyenikechukwudi@gmail.com>
2022-10-17 14:34:41 +0000

    main.h file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+#endif
